044a02d0a103144eff8164ffc53082229c78a9e5
dkorpics7 <dkorpics@roadrunner.com>
2017-05-07 02:24:24 -0400

    Formatted pages

--- MVCReviewProject/Models/Review.cs
+++ MVCReviewProject/Models/Review.cs
@@ -22,23 +22,26 @@ namespace MVCReviewProject.Models
         //FileLocation is for storing the url of an image (if I can figure out how to use it!)
         //SubmissionDate is the date the review is entered
 
-        [Display(Name = "Reviewer")]
+        [Display(Name = "Your Name")]
         public string Author { get; set; }
-        [Display(Name = "Name of Hotel/Restaurant/Attraction")]
+        [Display(Name = "Attraction/Hotel/Restaurant")]
         public string Attraction { get; set; }
         [Display(Name = "Date Visited")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime Date { get; set; }
         [Display(Name = "Headline")]
         public string Headline { get; set; }
         [Display(Name = "Review")]
+        [DataType(DataType.MultilineText)]
         public string Comment { get; set; }
         [Display(Name = "Overall Rating: 1-5")]
         public int Rating { get; set; }
         [Display(Name = "Price Rating: 1-5")]
         public int Price { get; set; }
-        [Display(Name = "Image URL")]
+        [Display(Name = "Rating (e.g., ***)")]
         public string FileLocation { get; set; }
-        [Display(Name = "Submission Date")]
+        [Display(Name = "Today's Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public Nullable<System.DateTime> SubmissionDate { get; set; }
 
 
